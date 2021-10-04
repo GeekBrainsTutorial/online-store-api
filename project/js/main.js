03a8c76c3ef7c533cc30f9a56cb1fbc0af8b3538
Nikitos-art <kurpasnikita@gmail.com>
2021-10-04 16:07:43 +0300

    project created, styles file added

---
+++ project/js/main.js
@@ -0,0 +1,22 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (title, price) => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item.title,item.price));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList;
+};
+
+renderPage(products);

d722dbcffec446332453c1e14b4d9851bb103b38
Nikitos-art <kurpasnikita@gmail.com>
2021-10-11 22:54:40 +0300

    added getBasket.json

--- project/js/main.js
+++ project/js/main.js
@@ -1,111 +1,80 @@
-class ProductList{
-    constructor(container='.products'){
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+class ProductsList {
+    constructor(container = '.products'){
         this.container = container;
-        this.goods = [];
-        this._fetchProducts();
-        this.render();//вывод товаров на страницу
-        const reducer = (previousValue, currentValue) => previousValue + currentValue;
-        let goodsSum = goods.reduce(reducer);
+        this.goods = [];//массив товаров из JSON документа
+        this._getProducts()
+            .then(data => { //data - объект js
+                 this.goods = [...data];
+                 this.render()
+            });
     }
-
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts(){
+      
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
-
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
     render(){
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
-            const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-             block.innerHTML += item.render();
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+//            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
+
     }
 }
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
-        this.title = product.title;
-        this.id = product.id;
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
         this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
     render(){
-           return `<div class="product-item">
-                <img src="${this.img}">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="buy-btn">Купить</button>
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`
     }
 }
-let list = new ProductList();
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-class Basket{
-    addToBasket(price){
-        let basket = []
-
-        }
-    }
-    removeFromBasket(price){
-
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-class Hamburger{
-    constructor(price, calories){
-        this.size = price;
-        this.price = calories;
-    }
-}
-
-class Kitchen{
-    makeBurger(size){
-        let burger = new Hamburger(price, calories);
-        return burger
-    }
-}
-
-class Order{
-    constructor(size, extras, add_spicy){
-        this.big_small = size;
-        this.salad_cheese_potato = extras;
-        this.spicy = add_spicy;
-        this.info = []
-        this.createOrder();
-    }
-    createOrder(){
-        let size = ['big', 'small'];
-        let addings = ['salad', 'cheese', 'potato'];
-        let spicy = ['yes', 'no'];
-        let cooking = new Kitchen();
-        if (size == 'big'){
-            if (addings = 'salad' or 'cheese' or 'potato') {
-                if (spicy = 'yes') {
-                    info.push()
-                }else {
-
-                }
-            }else{
-
-            }
-        }else{
-
-        }
-    }
-}
-
-
-
-
-
-
-
+let list = new ProductsList();
+console.log(list.allProducts);
 
